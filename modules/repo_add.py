75df2c9ce1626fc76605ef5217658a60c1f50b26
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-01-24 13:14:39 +0300

    add block

--- modules/repo_add.py
+++ modules/repo_add.py
@@ -12,9 +12,52 @@ class RepoAdd(commands.Cog):
         self.bot = bot
 
     @commands.group()
-    async def submission(self, ctx):
+    async def block(self, ctx):
         if ctx.invoked_subcommand is None:
             pass
+
+    @block.command()
+    @commands.has_any_role(*config.roles.approvers)
+    async def add(self, member_id: str):
+        try:
+            int(member_id)
+        except ValueError:
+            return await ctx.reply(embed=embed.error("Invalid member id"), mention_author=False)
+        if len(str(member_id)) != 17:
+            return await ctx.reply(embed=embed.error("Invalid member id"), mention_author=False)
+        blocked = config.db.get("blocked", default=[])
+        if member_id in blocked:
+            return await ctx.reply(embed=embed.error("Member already blocked"), mention_author=False)
+        blocked.append(member_id)
+        config.db.set("blocked", blocked)
+        await ctx.reply(embed=embed.success("Member successfully blocked"), mention_author=False)
+
+    @block.command()
+    @commands.has_any_role(*config.roles.approvers)
+    async def remove(self, member_id: str):
+        try:
+            int(member_id)
+        except ValueError:
+            return await ctx.reply(embed=embed.error("Invalid member id"), mention_author=False)
+        if len(str(member_id)) != 17:
+            return await ctx.reply(embed=embed.error("Invalid member id"), mention_author=False)
+        blocked = config.db.get("blocked", default=[])
+        if member_id not in blocked:
+            return await ctx.reply(embed=embed.error("Member is not blocked what are you doing"), mention_author=False)
+        blocked.remove(member_id)
+        config.db.set("blocked", blocked)
+        await ctx.reply(embed=embed.success("Member successfully unblocked"), mention_author=False)
+
+    @block.command(name="list")
+    @commands.has_any_role(*config.roles.approvers)
+    async def _list(self, member_id: str):
+        blocked_members = config.db.get("blocked", default=[])
+        descstr
+        for member_id in blocked_members:
+            descstr += f"{member_id} - <@{member_id}>\n"
+        if descstr == "":
+            descstr = "*No members blocked*"
+        await ctx.reply(embed=embed.success("Successfully fetched the blocked members list", descstr))
 
     @commands.command()
     async def submit(self, ctx, link: str):
@@ -28,7 +71,6 @@ class RepoAdd(commands.Cog):
         channel = ctx.guild.get_channel(config.channels.draft)
         await channel.send(f"Submission: <{link}>\nAuthor: {ctx.author.mention}\n`-submission (approve|reject) {sID}`", allowed_mentions=None)
         return await ctx.reply(embed=embed.success("Link submitted", "Please wait for it to be approved"), mention_author=False)
-        
 
     @commands.command()
     async def submissions(self, ctx):
@@ -52,6 +94,11 @@ class RepoAdd(commands.Cog):
             response_str = "\n".join(response_list)
         await ctx.reply(embed=embed.success("Successfully fetched the first 10 draft submissions", response_str[:-1]), mention_author=False)
 
+    @commands.group()
+    async def submission(self, ctx):
+        if ctx.invoked_subcommand is None:
+            pass
+
     @submission.command()
     @commands.has_any_role(*config.roles.approvers)
     async def approve(self, ctx, sID: str):
@@ -62,7 +109,8 @@ class RepoAdd(commands.Cog):
         if link in config.internal.get_devlist():
             return await ctx.reply(content="<@!287555395151593473> <@!512640455834337290>", embed=embed.error("That link is already in the database???", f"Submission: {link}"), mention_author=False)
         apitoken = os.getenv("apitoken")
-        response = requests.get(f"https://mantikralligi1.pythonanywhere.com/addDeveloper?token={apitoken}&githuburl={link}").text
+        response = requests.get(
+            f"https://mantikralligi1.pythonanywhere.com/addDeveloper?token={apitoken}&githuburl={link}").text
         if response != "Success":
             return await ctx.reply(content="<@!287555395151593473> <@!512640455834337290>", embed=embed.error("Server is offline", "Try again later"), mention_author=False)
         config.sboard.approve_suggestion(sID)
@@ -74,7 +122,7 @@ class RepoAdd(commands.Cog):
         member = await ctx.guild.fetch_member(author_id)
         try:
             await member.send(embed=embed.success("Your submission has been approved",
-                        f"Submission: {content}"))
+                                                  f"Submission: {content}"))
         except (discord.Forbidden, discord.HTTPException):
             pass
         channel = ctx.guild.get_channel(config.channels.approved)
@@ -94,7 +142,7 @@ class RepoAdd(commands.Cog):
         member = await ctx.guild.fetch_member(author_id)
         try:
             await member.send(embed=embed.error("Your submission has been rejected",
-                        f"Submission: {content}"))
+                                                f"Submission: {content}"))
         except (discord.Forbidden, discord.HTTPException):
             pass
         channel = ctx.guild.get_channel(config.channels.rejected)
